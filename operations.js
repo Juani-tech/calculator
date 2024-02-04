50f26be8074062d9355dc16bdbebc456b50f114a
Juani-tech <jperezdichiazza@gmail.com>
2024-02-04 17:26:01 -0300

    added all the necessary logic, hierarchy of operations and css

--- operations.js
+++ operations.js
@@ -8,16 +8,64 @@ function operate(operator, a , b){
             return a + b;
         case '-':
             return a - b;
-        case '*':
+        case 'x':
             return a * b;
         case '/':
+            if (b === 0){
+                return 'Error';
+            }
             return a / b;
     }
 }
 
-const button = document.querySelectorAll('button');
+
+function operations(operationsArray){
+    if (operationsArray.length === 3){
+        return operate(operationsArray[1], parseInt(operationsArray[0]), parseInt(operationsArray[2]));
+    }
+    if (operationsArray[1] === '-') {
+        if (operationsArray[3] == '+' || operationsArray[3] == '-'){
+            const res = operate(operationsArray[1], parseInt(operationsArray[0]), parseInt(operationsArray[2]));
+            operationsArray.splice(0, 3, res);
+            return operations(operationsArray);
+        }
+        return parseInt(operationsArray[0]) -  operations(operationsArray.slice(2));
+
+    } else if (operationsArray[1] === '+') {
+        if (operationsArray[3] == '+' || operationsArray[3] == '-'){
+            const res = operate(operationsArray[1], parseInt(operationsArray[0]), parseInt(operationsArray[2]));
+            operationsArray.splice(0, 3, res);
+            return operations(operationsArray);
+        }
+        return parseInt(operationsArray[0]) +  operations(operationsArray.slice(2));
+    }
+    
+    const res = operate(operationsArray[1], parseInt(operationsArray[0]), parseInt(operationsArray[2]));
+    operationsArray.splice(0, 3, res);
+    return operations(operationsArray);
+}
+
+
+const button = document.querySelectorAll('.number, .operand');
 button.forEach((button) => {
     button.addEventListener('click', () => {
         display.value = display.value + button.textContent;
     });
 });
+
+
+const equal = document.querySelector('.equal');
+
+equal.addEventListener('click', () => {
+    let displayValueAsArray = display.value.split(/(\+|\-|\x|\/)/g);
+    val = operations(displayValueAsArray);
+    display.value = val;
+});
+
+
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    display.value = '';
+});
+
+
